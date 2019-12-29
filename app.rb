51ee50f440c22f5d679c9edbdeafe7ae0c82dede
Ellie <elliemmanifold@gmail.com>
2019-12-29 17:03:34 +0000

    Set up app routes

---
+++ app.rb
@@ -0,0 +1,165 @@
+require 'sinatra/base'
+require './lib/game.rb'
+require './lib/question.rb'
+
+class Capitals < Sinatra::Base
+
+  get '/' do
+    $game = Game.new
+    @game = $game
+    question = Question.new
+    @new_question = question.generate
+    @game.question(@new_question)
+    erb :Q1
+  end
+
+  post '/Q1' do
+    @game = $game
+    @game.answer(params[:answer])
+    @game.result
+    redirect '/Q2'
+  end
+
+  get '/Q2' do
+    @game = $game
+    question = Question.new
+    @new_question = question.generate
+    @game.question(@new_question)
+    erb :Q2
+  end
+
+  post '/Q2' do
+    @game = $game
+    @game.answer(params[:answer])
+    @game.result
+    redirect '/Q3'
+  end
+
+  get '/Q3' do
+    @game = $game
+    question = Question.new
+    @new_question = question.generate
+    @game.question(@new_question)
+    erb :Q3
+  end
+
+  post '/Q3' do
+    @game = $game
+    @game.answer(params[:answer])
+    @game.result
+    p @game.rounds
+    p @game.questions
+    redirect '/Q4'
+  end
+
+  get '/Q4' do
+    @game = $game
+    question = Question.new
+    @new_question = question.generate
+    @game.question(@new_question)
+    erb :Q4
+  end
+
+  post '/Q4' do
+    @game = $game
+    @game.answer(params[:answer])
+    @game.result
+    redirect '/Q5'
+  end
+
+  get '/Q5' do
+    @game = $game
+    question = Question.new
+    @new_question = question.generate
+    @game.question(@new_question)
+    erb :Q5
+  end
+
+  post '/Q5' do
+    @game = $game
+    @game.answer(params[:answer])
+    @game.result
+    redirect '/Q6'
+  end
+
+  get '/Q6' do
+    @game = $game
+    question = Question.new
+    @new_question = question.generate
+    @game.question(@new_question)
+    erb :Q6
+  end
+
+  post '/Q6' do
+    @game = $game
+    @game.answer(params[:answer])
+    @game.result
+    redirect '/Q7'
+  end
+
+  get '/Q7' do
+    @game = $game
+    question = Question.new
+    @new_question = question.generate
+    @game.question(@new_question)
+    erb :Q7
+  end
+
+  post '/Q7' do
+    @game = $game
+    @game.answer(params[:answer])
+    @game.result
+    redirect '/Q8'
+  end
+
+  get '/Q8' do
+    @game = $game
+    question = Question.new
+    @new_question = question.generate
+    @game.question(@new_question)
+    erb :Q8
+  end
+
+  post '/Q8' do
+    @game = $game
+    @game.answer(params[:answer])
+    @game.result
+    redirect '/Q9'
+  end
+
+  get '/Q9' do
+    @game = $game
+    question = Question.new
+    @new_question = question.generate
+    @game.question(@new_question)
+    erb :Q9
+  end
+
+  post '/Q9' do
+    @game = $game
+    @game.answer(params[:answer])
+    @game.result
+    redirect '/Q10'
+  end
+
+  get '/Q10' do
+    @game = $game
+    question = Question.new
+    @new_question = question.generate
+    @game.question(@new_question)
+    erb :Q10
+  end
+
+  post '/Q10' do
+    @game = $game
+    @game.answer(params[:answer])
+    @game.result
+    redirect '/results'
+  end
+
+  get '/results' do
+    @game = $game
+  end
+
+  run! if app_file == $0
+end
--- lib/game.rb
+++ lib/game.rb
@@ -1,24 +1,28 @@
 class Game
-  attr_reader :score, :rounds
+  attr_reader :score, :rounds, :questions
 
   def initialize
     @score = 0
     @rounds = 0
+    @questions = []
+    @answers = []
   end
 
   def question(question)
     @question = question
+    @questions << @question
   end
 
   def answer(answer)
     @answer = answer
+    @answers << @answer
   end
 
   def result
-    if @question == @answer
+    @rounds += 1
+
+    if @answer == @question[1]
       @score += 1
-      @rounds += 1
-      true
     end
   end
 
